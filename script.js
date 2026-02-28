53e87af2bb1d67f6661840f221077a94f145b0f7
Clemens Prerovsky <c.prerovsky@gmail.com>
2026-02-28 06:35:49 +0100

    Update Google Apps Script URL in script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Replace this URL with your Google Apps Script Web App URL
 // See README.md for setup instructions
-const APPS_SCRIPT_URL = 'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby8LQeRPaO6lS5Rnml9CsWIDmpjLckzTMNEndKDhqt1URntP-CtXBscVYFrRUmRe0Ay/exec';
 
 document.getElementById('waitlistForm').addEventListener('submit', async function(e) {
   e.preventDefault();
